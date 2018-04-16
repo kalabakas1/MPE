f4bf86af53187bf6d0a450f6e845c7c6f6bf20e1
Mads Pedersen (MPE) <mpe@novicell.dk>
2018-04-16 18:18:05 +0200

    Developers guide

---
+++ Documentation/DevelopersGuide.md
@@ -0,0 +1,20 @@
+# Developers guide for an easy day
+A lot of things can be done to make a developers life easier, and make him focus on the one thing he needs to do. Develop software!. This is a short description of some of the things you can do to make your fellow developers life easier on a daily basis.
+
+## Motivation
+At one point I got tired of correcting stuff just to make the solution I worked on compile. This included configurations that didn't exist in the configuration files. Some of them didn't have transformations for the different environments, so they got deployed with the development values, which basically crashed the site. At the end of that day I started creating this document to make my life easier and to sum up what could be avoided of small irritating errors that might occur.
+
+## The List!!!
+It is gonna be a small list that sums up the different things along with an argument that sort of legalizes it. Not gonna be religious about it, but it's something I have gotten frustrated over, which could be avoided.
+
+### Remember to add configuration transformation items - you create, you trasnform
+When dealing with web applications or normal executables you might need to add something like appSettings in the .config files to differentiate between development, staging and the release environment. I spend around two hours trying to make all the configurations have the correct values, just because the developer that created didn't choose the correct values for the different environments.  You might not know the values, but you should know if it something that is gonna be transformed when building a specific environment.
+
+### Develope in branches - master should always be production ready
+There is a lot of hype regarding how to do software development, but in the current state of my work and employment it is still a best practice to create a branch if you have to do anything else then creating markdown files for documentation or something similar trivial. If you fixes a bug in production, creating a new feature - please do create a branch for it, and merge master into your branch on a regular basis.
+
+### Nothing is done before it have been tested on QA/staging/some-other-test-environment-name
+It's pretty simple, unless you have pushed it to a testing environment where you can see it works - then it is not done. When you have tested it on a test environment, then you should also have considered configuration transformations for at least that environment. After that you can mark the case as done and move on to the next interesting bit of your day.
+
+## Conclusion
+All teams are different and this small list of stuff can't fit all teams or projects. But the essence of it is the same. Identify the irritating repetitive errors that occurs, find a solution to them, and implement the solution as a concrete deployment rule, or a culture in the team. You can even game-ify it by having a cake/beer rule - for each time a certain error occurs, the developer should bring it for the next team meeting.
--- MPE/Program.cs
+++ MPE/Program.cs
@@ -1,57 +1,75 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Collections.Specialized;
 using System.Configuration;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
-using MongoDB.Driver;
-using MPE.Models;
-using MPE.MongoDB.Logic;
-using MPE.MongoDB.Repositories;
-using NPoco;
+using Newtonsoft.Json;
 
 namespace MPE
 {
     class Program
     {
-        private static Initializer _initialization;
+        private static HttpClient _client = new HttpClient();
         static void Main(string[] args)
         {
-            _initialization = new Initializer();
-            _initialization.Start();
+            var data = JsonConvert.DeserializeObject<List<Dictionary<string, string>>>(
+                File.ReadAllText(@"C:\Users\mpe\Desktop\data.json"));
 
-            AppDomain.CurrentDomain.ProcessExit += OnProcessExit;
+            Console.WriteLine("Data read...");
 
-            Console.WriteLine("Started...");
 
-            var objs = new List<Member>();
-            using (var db = new Database("VenmaDb"))
+            int count = 0;
+            foreach (var d in data)
             {
-                objs = db.Fetch<Member>("SELECT CreatedOn, MemberID AS EntityId, Firstname, Lastname FROM Member").ToList();
-            }
+                try
+                {
+                    Send("", d);
+                    Thread.Sleep(50);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine($"Error: {e.Message}");
+                }
+                count++;
 
-            var repo = new MongoRepositoryBase<Member>();
-
-            var all = repo.GetAll();
-            foreach (var member in all)
-            {
-                repo.Delete(member);
-            }
-
-            foreach (var member in objs)
-            {
-                repo.Save(member);
+                if (count % 100 == 0)
+                {
+                    Console.WriteLine(count);
+                }
             }
 
             Console.WriteLine("done...");
-
             Console.ReadLine();
         }
 
-        static void OnProcessExit(object sender, EventArgs e)
+        private static async void Send(string url, Dictionary<string, string> data)
         {
-            _initialization.Stop();
+            var fields = new NameValueCollection();
+            foreach (KeyValuePair<string, string> d in data)
+            {
+                fields.Add(d.Key, d.Value.ToString());
+            }
+
+            try
+            {
+                var response = await _client.PostAsync(url, new FormUrlEncodedContent(data.Select(x => x)));
+                var strData = response.Content.ReadAsStringAsync().Result;
+                if (!string.IsNullOrEmpty(strData))
+                {
+                    Console.WriteLine(strData);
+                }
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
         }
     }
 }
