019658eba5db9f26052c771193337600a8db4df0
Mads Pedersen (MPE) <mpe@novicell.dk>
2018-06-10 13:15:56 +0200

    Pinger queries

---
+++ MPE.Pinger/Configuration/Queries/CaptureStatusDistribution.sql
@@ -0,0 +1,10 @@
+﻿SELECT 
+	t.CaptureStatusName AS Alias
+	, COUNT(*) AS Value
+FROM 
+	[Transaction] t WITH (NOLOCK) 
+	JOIN OrderLine ol WITH(NOLOCK) ON t.OrderLineID = ol.OrderLineID AND t.Deleted = 0 AND ol.Deleted = 0 AND t.CaptureStatusName IS NOT NULL
+	JOIN [Order] o WITH (NOLOCK) ON o.OrderID = ol.OrderID AND o.Deleted = 0 
+	JOIN Season s WITH (NOLOCK) ON o.SeasonID = s.SeasonID AND s.[From] <= GETDATE() AND s.[To] >= GETDATE() AND s.Deleted = 0 AND s.IsActive = 1
+GROUP BY
+	t.CaptureStatusName
---
+++ MPE.Pinger/Configuration/Queries/TeamMemberStatusVariationDistribution.sql
@@ -0,0 +1,12 @@
+﻿SELECT 
+	CONCAT(v.Alias,'.',tms.Alias) AS 'Alias'
+	, COUNT(*) AS Value
+FROM
+	TeamMember tm WITH (NOLOCK)
+	JOIN Team t WITH(NOLOCK) ON t.TeamID = tm.TeamID AND tm.Deleted = 0 AND t.Deleted = 0
+	JOIN TeamMemberStatus tms WITH(NOLOCK) ON tms.TeamMemberStatusID = tm.TeamMemberStatusID AND tms.Deleted = 0
+	JOIN Variety v WITH(NOLOCK) ON t.TeamVarietyID = v.VarietyID AND v.Deleted = 0
+	JOIN Season s WITH(NOLOCK) ON t.SeasonID = s.SeasonID AND s.[From] <= GETDATE() AND s.[To] >= GETDATE() AND s.Deleted = 0 AND s.IsActive = 1
+GROUP BY
+	v.Alias
+	, tms.Alias
